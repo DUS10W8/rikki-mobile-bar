cdab0ea4e3e760575a845af432857b053f391bf1
DUS10W8 <dwaite8@wgu.edu>
2025-09-10 19:03:19 -0700

    Add src/lib/utils and deps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/** Merge Tailwind classes safely */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
