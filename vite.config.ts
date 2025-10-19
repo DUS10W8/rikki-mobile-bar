eb14782e4059906b2a60c26166d4dae5c1c13e08
Dustin Waite <137970251+DUS10W8@users.noreply.github.com>
2025-10-19 14:15:07 -0700

    Update path alias resolution in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  // your GitHub Pages project base
-  base: "/",
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)), // so "@/..." points to src/
+      "@": path.resolve(__dirname, "src"),
     },
   },
+  base: "/",
 });
